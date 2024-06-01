3fae43f9e1c6a0543ce17c3c355e7f1226bf35d2
Tarnak Shinde <shindetarnak@example.com>
2024-06-01 13:25:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://smartbrain-back-v9s9.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/register", {
+    fetch("https://smartbrain-back-v9s9.onrender.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
